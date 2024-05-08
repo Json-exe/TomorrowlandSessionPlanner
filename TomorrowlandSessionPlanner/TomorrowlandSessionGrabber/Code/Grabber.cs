e66336103b43ceff03261a0057265102e06379a2
Json-exe <lucaheitfeld@gmail.com>
2024-05-08 21:13:20 +0200

    feat: Fix migrations, grabber now works when no times are given on line up, updated DB with weekend 2 data 2024, updated packages

--- TomorrowlandSessionPlanner/TomorrowlandSessionGrabber/Code/Grabber.cs
+++ TomorrowlandSessionPlanner/TomorrowlandSessionGrabber/Code/Grabber.cs
@@ -1,7 +1,8 @@
 ﻿using System.Collections.ObjectModel;
+using System.Globalization;
 using Newtonsoft.Json;
 using OpenQA.Selenium;
-using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
 using TomorrowlandSessionGrabber.Models;
 
 namespace TomorrowlandSessionGrabber.Code;
@@ -10,23 +11,23 @@ public class Grabber
 {
     private const string BaseUrl = "https://www.tomorrowland.com/en/festival/line-up/stages/";
     private ReadOnlyCollection<IWebElement>? _eventDayButtons;
-    private FirefoxDriver _driver = null!;
+    private IWebDriver _driver = null!;
     private int _selectedDay;
     private readonly List<Session> _sessions = [];
 
     public void Start(GrabberOptions grabberOptions)
     {
         Console.WriteLine("Starting grabber...");
-        var options = new FirefoxOptions();
+        var options = new ChromeOptions();
         if (grabberOptions.Headless)
         {
             options.AddArguments("--headless");
         }
 
-        var driver = new FirefoxDriver(options);
+        var driver = new ChromeDriver(options);
         _driver = driver;
         driver.Navigate().GoToUrl(BaseUrl);
-        driver.ExecuteScript("Cookiebot.dialog.submitConsent()");
+        // driver.ExecuteScript("Cookiebot.dialog.submitConsent()");
         if (grabberOptions.Weekend == Weekend.Weekend1)
         {
             var weekendSwitch = driver.FindElement(By.ClassName("weekend-switch"));
@@ -50,6 +51,7 @@ public class Grabber
             Console.WriteLine("Grabbing weekend 2...");
             GetStages();
         }
+
         SaveDataAsJson();
     }
 
@@ -60,6 +62,7 @@ public class Grabber
         {
             File.Delete("sessionGrabberSessions.json");
         }
+
         var json = JsonConvert.SerializeObject(_sessions, Formatting.Indented);
         File.WriteAllText("sessionGrabberSessions.json", json);
     }
@@ -97,78 +100,93 @@ public class Grabber
                     StageName = heading.Text
                 };
                 var djData = stageContent.FindElement(By.TagName("a"));
-                try
+
+                ApplySessionTimeStampIfAvailable(session, djData);
+
+                var djName = djData.Text;
+                // DJ Name CAN look like this: Daybreak Session: Claptone|\r\n12:00 - 13:00
+                // We want only the Name: Claptone
+                var djNameParts = djName.Split("\r");
+                // Also the part in front of the name must be removed
+                if (!djNameParts[0].Contains("KAS:ST") && !djNameParts[0].Contains("Myu:sa"))
                 {
-                    var timeSlot = djData.FindElement(By.TagName("span"));
-                    var timeSlotParts = timeSlot.Text.Split(" - ");
-                    // The date is not in the time slot, so we need to get it from the event day button. The event day button has a data-date attribute with the date
-                    var date = _eventDayButtons![_selectedDay].GetAttribute("data-date");
-                    // Format is: 2023/07/28
-                    var dateParts = date.Split("/");
-                    var endParts = dateParts.ToArray();
-                    var startTime = TimeSpan.Parse(timeSlotParts[0]);
-                    var endTime = TimeSpan.Parse(timeSlotParts[1]);
-
-                    // Check if end or start time is after midnight. If so, add a day to the date
-                    if (endTime < startTime)
-                    {
-                        // End time is after midnight, so add a day to the date
-                        var dateTime = new DateTime(int.Parse(endParts[0]), int.Parse(endParts[1]),
-                            int.Parse(endParts[2]));
-                        dateTime = dateTime.AddDays(1);
-                        endParts[0] = dateTime.Year.ToString();
-                        endParts[1] = dateTime.Month.ToString();
-                        endParts[2] = dateTime.Day.ToString();
-                    }
-
-                    if (endTime >= new TimeSpan(0, 0, 0) && startTime >= new TimeSpan(0, 0, 0)
-                        && endTime<= new TimeSpan(2, 0, 0) && startTime <= new TimeSpan(2, 0, 0))
-                    {
-                        // Start and end time are between 00:00 and 02:00, so add a day to both dates
-                        var dateTime = new DateTime(int.Parse(endParts[0]), int.Parse(endParts[1]),
-                            int.Parse(endParts[2]));
-                        dateTime = dateTime.AddDays(1);
-                        endParts[0] = dateTime.Year.ToString();
-                        endParts[1] = dateTime.Month.ToString();
-                        endParts[2] = dateTime.Day.ToString();
-                        
-                        dateTime = new DateTime(int.Parse(dateParts[0]), int.Parse(dateParts[1]),
-                            int.Parse(dateParts[2]));
-                        dateTime = dateTime.AddDays(1);
-                        dateParts[0] = dateTime.Year.ToString();
-                        dateParts[1] = dateTime.Month.ToString();
-                        dateParts[2] = dateTime.Day.ToString();
-                    }
-
-                    session.StartTime = new DateTime(int.Parse(dateParts[0]), int.Parse(dateParts[1]),
-                        int.Parse(dateParts[2]), startTime.Hours, startTime.Minutes, startTime.Seconds);
-                    session.EndTime = new DateTime(int.Parse(endParts[0]), int.Parse(endParts[1]),
-                        int.Parse(endParts[2]), endTime.Hours, endTime.Minutes, endTime.Seconds);
-
-                    var djName = djData.Text;
-                    // DJ Name CAN look like this: Daybreak Session: Claptone|\r\n12:00 - 13:00
-                    // We want only the Name: Claptone
-                    var djNameParts = djName.Split("\r");
-                    // Also the part in front of the name must be removed
-                    if (!djNameParts[0].Contains("KAS:ST") && !djNameParts[0].Contains("Myu:sa"))
-                    {
-                        var djNameParts2 = djNameParts[0].Split(":");
-                        djName = djNameParts2.Length > 1 ? djNameParts2[1] : djNameParts2[0];
-                    }
-                    else
-                    {
-                        djName = djNameParts[0];
-                    }
-                    session.DjName = djName.Trim();
-                    Console.WriteLine(
-                        $"Stage: {session.StageName}, DJ: {session.DjName}, Start: {session.StartTime}, End: {session.EndTime}");
-                    _sessions.Add(session);
+                    var djNameParts2 = djNameParts[0].Split(":");
+                    djName = djNameParts2.Length > 1 ? djNameParts2[1] : djNameParts2[0];
                 }
-                catch (Exception e)
+                else
                 {
-                    Console.WriteLine("Time element not found. Skipping. Or maybe other error: " + e.Message);
+                    djName = djNameParts[0];
                 }
+
+                session.DjName = djName.Trim();
+                Console.WriteLine(
+                    $"Stage: {session.StageName}, DJ: {session.DjName}, Start: {session.StartTime}, End: {session.EndTime}");
+                _sessions.Add(session);
             }
+        }
+    }
+
+    private void ApplySessionTimeStampIfAvailable(Session session, IWebElement djData)
+    {
+        try
+        {
+            var timeSlot = djData.FindElement(By.TagName("span"));
+            var timeSlotParts = timeSlot.Text.Split(" - ");
+            // The date is not in the time slot, so we need to get it from the event day button. The event day button has a data-date attribute with the date
+            var date = _eventDayButtons![_selectedDay].GetAttribute("data-date");
+            // Format is: 2023/07/28
+            var dateParts = date.Split("/");
+            var endParts = dateParts.ToArray();
+            var startTime = TimeSpan.Parse(timeSlotParts[0], CultureInfo.InvariantCulture);
+            var endTime = TimeSpan.Parse(timeSlotParts[1], CultureInfo.InvariantCulture);
+
+            // Check if end or start time is after midnight. If so, add a day to the date
+            if (endTime < startTime)
+            {
+                // End time is after midnight, so add a day to the date
+                var dateTime = new DateTime(int.Parse(endParts[0]), int.Parse(endParts[1]),
+                    int.Parse(endParts[2]));
+                dateTime = dateTime.AddDays(1);
+                endParts[0] = dateTime.Year.ToString();
+                endParts[1] = dateTime.Month.ToString();
+                endParts[2] = dateTime.Day.ToString();
+            }
+
+            if (endTime >= new TimeSpan(0, 0, 0) && startTime >= new TimeSpan(0, 0, 0)
+                                                 && endTime <= new TimeSpan(2, 0, 0) &&
+                                                 startTime <= new TimeSpan(2, 0, 0))
+            {
+                // Start and end time are between 00:00 and 02:00, so add a day to both dates
+                var dateTime = new DateTime(int.Parse(endParts[0]), int.Parse(endParts[1]),
+                    int.Parse(endParts[2]));
+                dateTime = dateTime.AddDays(1);
+                endParts[0] = dateTime.Year.ToString();
+                endParts[1] = dateTime.Month.ToString();
+                endParts[2] = dateTime.Day.ToString();
+
+                dateTime = new DateTime(int.Parse(dateParts[0]), int.Parse(dateParts[1]),
+                    int.Parse(dateParts[2]));
+                dateTime = dateTime.AddDays(1);
+                dateParts[0] = dateTime.Year.ToString();
+                dateParts[1] = dateTime.Month.ToString();
+                dateParts[2] = dateTime.Day.ToString();
+            }
+
+            session.StartTime = new DateTime(int.Parse(dateParts[0]), int.Parse(dateParts[1]),
+                int.Parse(dateParts[2]), startTime.Hours, startTime.Minutes, startTime.Seconds);
+            session.EndTime = new DateTime(int.Parse(endParts[0]), int.Parse(endParts[1]),
+                int.Parse(endParts[2]), endTime.Hours, endTime.Minutes, endTime.Seconds);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Error applying time stamp: " + e.Message +
+                              ". Adding session without time stamp. Applying only date for now...");
+            var date = _eventDayButtons![_selectedDay].GetAttribute("data-date");
+            var dateParts = date.Split("/");
+            session.StartTime = new DateTime(int.Parse(dateParts[0]), int.Parse(dateParts[1]),
+                int.Parse(dateParts[2]));
+            session.EndTime = new DateTime(int.Parse(dateParts[0]), int.Parse(dateParts[1]),
+                int.Parse(dateParts[2]));
         }
     }
 }
